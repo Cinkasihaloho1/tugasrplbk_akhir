0d01468dda26493adc8c7e400ae995d4c19ea7c8
Cinkasihaloho1 <cinkasihaloho@gmail.com>
2022-12-08 18:52:13 +0700

    first commit

---
+++ src/views/AboutPage.js
@@ -0,0 +1,15 @@
+// material-ui
+import { Typography } from '@mui/material';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| SAMPLE PAGE ||============================== //
+
+const AboutPage = () => (
+    <MainCard title="Tentang Aplikasi">
+        <h4>About app</h4>
+    </MainCard>
+);
+
+export default AboutPage;
---
+++ src/views/dashboard/Default/ProfileCard.js
@@ -0,0 +1,20 @@
+// material-ui
+import { Typography } from '@mui/material';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| SAMPLE PAGE ||============================== //
+
+const ProfileCard = () => (
+    <MainCard title="Tugas Akhir RPLBK Kelompok 29">
+        <ol>
+            <li>Nama</li>
+            <li>Nama</li>
+            <li>Nama</li>
+            <li>Nama</li>
+        </ol>
+    </MainCard>
+);
+
+export default ProfileCard;
